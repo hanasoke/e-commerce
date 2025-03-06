5a2622b6ebc2b6034bcd18d3e86de99110fbffed
hanasoke@gmail.com <hanasoke@gmail.com>
2025-03-06 10:26:20 +0700

    delete all folders

--- main.rb
+++
@@ -1,6 +0,0 @@
-# puts "Hello, Hanas Bayu Pratama, B.SC"
-
-require_relative 'lib/my_class'
-
-my_instance = MyClass.new 
-my_instance.fetch_data
--- spec/my_class_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative "../lib/my_class"
-
-RSpec.describe MyClass do 
-    it 'fetches data from the database' do 
-        my_instance = MyClass.new 
-        expect {my_instance.fetch_data }.to output.to_stdout
-    end 
-end 
