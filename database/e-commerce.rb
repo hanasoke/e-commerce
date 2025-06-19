a070efbf5746044e82c0749a43a8bfb89c9decfd
Hanas <hanasoke@gmail.com>
2025-06-19 19:07:15 +0700

    update layout pages

---
+++ database/e-commerce.rb
@@ -0,0 +1,21 @@
+require 'sqlite3'
+
+DB = SQLite3::Database.new('e-commerce.db')
+DB.results_as_hash = true
+
+# Profiles Table 
+DB.execute <<-SQL 
+    CREATE TABLE IF NOT EXISTS e-commerce (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        username TEXT UNIQUE,
+        email TEXT UNIQUE,
+        password TEXT,
+        birthdate TEXT,
+        address TEXT,
+        phone INT,
+        photo TEXT
+        reset_token TEXT
+        access INTEGER
+    );
+SQL
--- main.rb
+++ main.rb
@@ -1,1 +1,34 @@
-puts "Hello, Ruby Project!"
+require 'sinatra'
+require 'sinatra/flash'
+require 'bcrypt'
+require_relative 'database/e-commerce'
+require 'prawn'
+require 'rubyXL'
+require 'prawn/table'
+require 'write_xlsx'
+
+# This is also needed for generating files in memory
+require 'stringio'
+
+enable :sessions 
+register Sinatra::flash
+
+# Allow access from any IP 
+
+set :bind, '128.0.0.2'
+
+# Different server port
+set :port, 4002
+
+# Helper methods
+def logged_in?
+    session[:profile_id] != nil 
+end 
+
+def current_profile 
+    @current_profile ||= DB.execute("SELECT * FROM users WHERE user_id = ?", [session[:profile_id]]).first if logged_in?
+end 
+
+# Routes 
+get '/' do 
+    $title = ''
