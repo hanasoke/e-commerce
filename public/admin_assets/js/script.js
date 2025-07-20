4983ce14f78ced5de7dc1b3d304b55b0642b58b9
Hanas <hanasoke@gmail.com>
2025-07-20 15:46:23 +0700

    fixing password eye errors in register

--- public/admin_assets/js/script.js
+++ public/admin_assets/js/script.js
@@ -1,15 +1,14 @@
-
 function togglePassword(inputId, iconSpan) {
     const input = document.getElementById(inputId);
-    const icon = iconSpan.querySelector('i');
+    const icon = iconSpan.querySelector('.passwordeyes');
 
-    if(input.type === 'password') {
+    if (input.type === 'password') {
         input.type = 'text';
-        icon.classList.remove('fa-eye');
-        icon.classList.add('fa-eye-slash');
-    } else {
-        input.type = 'password';
         icon.classList.remove('fa-eye-slash');
         icon.classList.add('fa-eye');
+    } else {
+        input.type = 'password';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
     }
 }
