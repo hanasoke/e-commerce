f21c955bbd09e3b3edf1c631d85930ce251ecde4
Hanas <hanasoke@gmail.com>
2025-07-31 16:10:05 +0700

    update user_profile pages

--- main.rb
+++ main.rb
@@ -733,7 +733,7 @@ get '/user_profile/:user_id' do
     erb :'user/profile/view', layout: :'layouts/user/template'
 end 
 
-get '/editan' do 
+get '/user_profile_edit/:user_id' do 
     redirect '/login' unless logged_in? 
 
     @title = "User Profile Edit"
