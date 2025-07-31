8510989a20b4ee2569821c944f13300bc9da5e0a
Hanas <hanasoke@gmail.com>
2025-08-01 05:42:40 +0700

    adding seller_profile_edit function

--- main.rb
+++ main.rb
@@ -710,6 +710,26 @@ get '/seller_profile_view/:user_id' do
     erb :'seller/profile/view', layout: :'layouts/seller/template'
 end 
 
+get '/seller_profile_view/:user_id' do 
+    redirect '/login' unless logged_in? 
+
+    @title = "Seller Profile Edit"
+    @profile = current_user
+
+    @errors = []
+    erb :'seller/profile/edit', layout: :'layouts/seller/template'
+end 
+
+get '/seller_profile_edit/:user_id' do 
+    redirect '/login' unless logged_in? 
+
+    @title = "User Profile Edit"
+    @profile = current_user
+
+    @errors = []
+    erb :'user/profile/edit', layout: :'layouts/user/template'
+end 
+
 get '/user_profile/:user_id' do 
     redirect '/login' unless logged_in? 
 
