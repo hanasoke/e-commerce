f460c08aedd7295e02fd86937547838154a96861
Hanas <hanasoke@gmail.com>
2025-08-11 09:32:45 +0700

    creating stores table

--- database/e-commerce.rb
+++ database/e-commerce.rb
@@ -29,3 +29,16 @@ DB.execute <<-SQL
         FOREIGN KEY(user_id) REFERENCES users(user_id)
     );
 SQL
+
+# Store Table 
+DB.execute <<-SQL 
+    CREATE TABLE NOT EXISTS store (
+        store_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        seller_id INTEGER,
+        store_name TEXT,
+        store_photo TEXT,
+        store_address TEXT,
+        cs_number TEXT,
+        FOREIGN KEY(seller_id) REFERENCES sellers(seller_id)
+    )
+SQL
