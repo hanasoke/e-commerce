71b8779b920c89b86445ef971011ee6fc6773b90
Hanas <hanasoke@gmail.com>
2025-10-13 18:08:37 +0700

    added wishlists table

--- database/e-commerce.rb
+++ database/e-commerce.rb
@@ -65,19 +65,19 @@ SQL
 
 # DB.execute("DROP TABLE items");
 
-# DB.execute <<-SQL 
-#     CREATE TABLE IF NOT EXISTS wishlists (
-#         wishlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         item_id INTEGER,
-#         store_id INTEGER,
-#         user_id INTEGER,
-#         FOREIGN KEY(user_id) REFERENCES users(user_id),
-#         FOREIGN KEY(store_id) REFERENCES stores(store_id),
-#         FOREIGN KEY(item_id) REFERENCES items(item_id)
-#     );
-# SQL
+DB.execute <<-SQL 
+    CREATE TABLE IF NOT EXISTS wishlists (
+        wishlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_id INTEGER,
+        store_id INTEGER,
+        user_id INTEGER,
+        FOREIGN KEY(user_id) REFERENCES users(user_id),
+        FOREIGN KEY(store_id) REFERENCES stores(store_id),
+        FOREIGN KEY(item_id) REFERENCES items(item_id)
+    );
+SQL
 
-DB.execute("DROP TABLE wishlists");
+# DB.execute("DROP TABLE wishlists");
 
 DB.execute <<-SQL
     CREATE TABLE IF NOT EXISTS baskets (
