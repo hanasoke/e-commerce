71a31fc1ca924cf82e1048801dea90122387c219
Hanas <hanasoke@gmail.com>
2025-11-18 16:56:09 +0700

    drop all table in e-commerce.rb

--- database/e-commerce.rb
+++ database/e-commerce.rb
@@ -4,180 +4,186 @@ DB = SQLite3::Database.new('e_commerce.db')
 DB.results_as_hash = true
 
 # Profiles Table 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE,
-        username TEXT UNIQUE,
-        email TEXT UNIQUE,
-        password TEXT,
-        birthdate TEXT,
-        address TEXT,
-        phone INT,
-        photo TEXT,
-        reset_token TEXT,
-        access INTEGER
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS users (
+#         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT UNIQUE,
+#         username TEXT UNIQUE,
+#         email TEXT UNIQUE,
+#         password TEXT,
+#         birthdate TEXT,
+#         address TEXT,
+#         phone INT,
+#         photo TEXT,
+#         reset_token TEXT,
+#         access INTEGER
+#     );
+# SQL
+
+DB.execute("DROP TABLE users");
 
 # Sellers Table
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS sellers (
-        seller_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        identity_photo TEXT,
-        FOREIGN KEY(user_id) REFERENCES users(user_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS sellers (
+#         seller_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         user_id INTEGER,
+#         identity_photo TEXT,
+#         FOREIGN KEY(user_id) REFERENCES users(user_id)
+#     );
+# SQL
+
+DB.execute("DROP TABLE sellers");
 
 # Stores Table 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS stores (
-        store_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        seller_id INTEGER,
-        store_name TEXT,
-        store_photo TEXT,
-        store_banner TEXT,
-        store_address TEXT,
-        store_status TEXT,
-        cs_number TEXT,
-        FOREIGN KEY(seller_id) REFERENCES sellers(seller_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS stores (
+#         store_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         seller_id INTEGER,
+#         store_name TEXT,
+#         store_photo TEXT,
+#         store_banner TEXT,
+#         store_address TEXT,
+#         store_status TEXT,
+#         cs_number TEXT,
+#         FOREIGN KEY(seller_id) REFERENCES sellers(seller_id)
+#     );
+# SQL
+
+DB.execute("DROP TABLE stores");
 
 # Items Table
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS items (
-        item_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        store_id INTEGER,
-        item_name TEXT, 
-        item_brand TEXT,
-        item_photo TEXT,
-        item_description TEXT,
-        item_price INTEGER,
-        item_stock INTEGER,
-        item_category TEXT,
-        item_unit TEXT,
-        item_status TEXT,
-        FOREIGN KEY(store_id) REFERENCES stores(store_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS items (
+#         item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         store_id INTEGER,
+#         item_name TEXT, 
+#         item_brand TEXT,
+#         item_photo TEXT,
+#         item_description TEXT,
+#         item_price INTEGER,
+#         item_stock INTEGER,
+#         item_category TEXT,
+#         item_unit TEXT,
+#         item_status TEXT,
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id)
+#     );
+# SQL
 
-# DB.execute("DROP TABLE items");
+DB.execute("DROP TABLE items");
 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS wishlists (
-        wishlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        item_id INTEGER,
-        store_id INTEGER,
-        user_id INTEGER,
-        FOREIGN KEY(user_id) REFERENCES users(user_id),
-        FOREIGN KEY(store_id) REFERENCES stores(store_id),
-        FOREIGN KEY(item_id) REFERENCES items(item_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS wishlists (
+#         wishlist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         item_id INTEGER,
+#         store_id INTEGER,
+#         user_id INTEGER,
+#         FOREIGN KEY(user_id) REFERENCES users(user_id),
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id),
+#         FOREIGN KEY(item_id) REFERENCES items(item_id)
+#     );
+# SQL
 
-# DB.execute("DROP TABLE wishlists");
+DB.execute("DROP TABLE wishlists");
 
-DB.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS baskets (
-        basket_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        wishlist_id INTEGER,
-        item_id INTEGER,
-        store_id INTEGER,
-        user_id INTEGER,
-        quantity INTEGER,
-        total_price INTEGER,
-        note TEXT,
-        FOREIGN KEY(wishlist_id) REFERENCES wishlists(wishlist_id),
-        FOREIGN KEY(user_id) REFERENCES users(user_id),
-        FOREIGN KEY(store_id) REFERENCES stores(store_id),
-        FOREIGN KEY(item_id) REFERENCES items(item_id)
-    )
-SQL
+# DB.execute <<-SQL
+#     CREATE TABLE IF NOT EXISTS baskets (
+#         basket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         wishlist_id INTEGER,
+#         item_id INTEGER,
+#         store_id INTEGER,
+#         user_id INTEGER,
+#         quantity INTEGER,
+#         total_price INTEGER,
+#         note TEXT,
+#         FOREIGN KEY(wishlist_id) REFERENCES wishlists(wishlist_id),
+#         FOREIGN KEY(user_id) REFERENCES users(user_id),
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id),
+#         FOREIGN KEY(item_id) REFERENCES items(item_id)
+#     )
+# SQL
 
-# DB.execute("DROP TABLE baskets");
+DB.execute("DROP TABLE baskets");
 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS services (
-        service_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        service_name TEXT,
-        fee INTEGER
-    )
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS services (
+#         service_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         service_name TEXT,
+#         fee INTEGER
+#     )
+# SQL
 
-# DB.execute("DROP TABLE services");
+DB.execute("DROP TABLE services");
 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS store_services (
-        store_service_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        service_id INTEGER,
-        store_id INTEGER,
-        service_status TEXT,
-        FOREIGN KEY(service_id) REFERENCES services(service_id),
-        FOREIGN KEY(store_id) REFERENCES stores(store_id)   
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS store_services (
+#         store_service_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         service_id INTEGER,
+#         store_id INTEGER,
+#         service_status TEXT,
+#         FOREIGN KEY(service_id) REFERENCES services(service_id),
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id)   
+#     );
+# SQL
 
-# DB.execute("DROP TABLE store_services");
+DB.execute("DROP TABLE store_services");
 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS transactions (
-        transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        store_id INTEGER,
-        item_id INTEGER, 
-        user_id INTEGER,
-        wishlist_id INTEGER,
-        basket_id INTEGER,
-        store_service_id INTEGER,
-        quantity INTEGER,
-        total_price INTEGER,
-        payment_method TEXT,
-        account_number TEXT,
-        payment_photo TEXT,
-        payment_status TEXT,
-        transaction_date TEXT, 
-        note TEXT,
-        payment_name TEXT,
-        FOREIGN KEY(wishlist_id) REFERENCES wishlists(wishlist_id),
-        FOREIGN KEY(user_id) REFERENCES users(user_id),
-        FOREIGN KEY(store_id) REFERENCES stores(store_id),
-        FOREIGN KEY(item_id) REFERENCES items(item_id),
-        FOREIGN KEY(basket_id) REFERENCES baskets(basket_id),
-        FOREIGN KEY(store_service_id) REFERENCES store_services(store_service_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS transactions (
+#         transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         store_id INTEGER,
+#         item_id INTEGER, 
+#         user_id INTEGER,
+#         wishlist_id INTEGER,
+#         basket_id INTEGER,
+#         store_service_id INTEGER,
+#         quantity INTEGER,
+#         total_price INTEGER,
+#         payment_method TEXT,
+#         account_number TEXT,
+#         payment_photo TEXT,
+#         payment_status TEXT,
+#         transaction_date TEXT, 
+#         note TEXT,
+#         payment_name TEXT,
+#         FOREIGN KEY(wishlist_id) REFERENCES wishlists(wishlist_id),
+#         FOREIGN KEY(user_id) REFERENCES users(user_id),
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id),
+#         FOREIGN KEY(item_id) REFERENCES items(item_id),
+#         FOREIGN KEY(basket_id) REFERENCES baskets(basket_id),
+#         FOREIGN KEY(store_service_id) REFERENCES store_services(store_service_id)
+#     );
+# SQL
 
-# DB.execute("DROP TABLE transactions");
+DB.execute("DROP TABLE transactions");
 
-DB.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS shipments (
-        shipping_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        transaction_id INTEGER,
-        shipping_status TEXT,
-        shipping_date TEXT,
-        FOREIGN KEY(transaction_id) REFERENCES transactions(transaction_id)
-    );
-SQL
+# DB.execute <<-SQL
+#     CREATE TABLE IF NOT EXISTS shipments (
+#         shipping_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         transaction_id INTEGER,
+#         shipping_status TEXT,
+#         shipping_date TEXT,
+#         FOREIGN KEY(transaction_id) REFERENCES transactions(transaction_id)
+#     );
+# SQL
 
-# DB.execute("DROP TABLE shipments");
+DB.execute("DROP TABLE shipments");
 
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS messages (
-        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        store_id INTEGER,
-        user_id INTEGER,
-        sender_type TEXT, -- 'user' or 'seller'
-        message_text TEXT,
-        message_type TEXT DEFAULT 'text', -- 'text', 'image', 'product'
-        product_id INTEGER, -- if message_type is 'product'
-        image_path TEXT, -- if message_type is 'image'
-        is_read BOOLEAN DEFAULT FALSE,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY(store_id) REFERENCES stores(store_id),
-        FOREIGN KEY(user_id) REFERENCES users(user_id),
-        FOREIGN KEY(product_id) REFERENCES items(item_id)
-    );
-SQL
+# DB.execute <<-SQL 
+#     CREATE TABLE IF NOT EXISTS messages (
+#         message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         store_id INTEGER,
+#         user_id INTEGER,
+#         sender_type TEXT, -- 'user' or 'seller'
+#         message_text TEXT,
+#         message_type TEXT DEFAULT 'text', -- 'text', 'image', 'product'
+#         product_id INTEGER, -- if message_type is 'product'
+#         image_path TEXT, -- if message_type is 'image'
+#         is_read BOOLEAN DEFAULT FALSE,
+#         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+#         FOREIGN KEY(store_id) REFERENCES stores(store_id),
+#         FOREIGN KEY(user_id) REFERENCES users(user_id),
+#         FOREIGN KEY(product_id) REFERENCES items(item_id)
+#     );
+# SQL
 
-# DB.execute("DROP TABLE messages");
+DB.execute("DROP TABLE messages");
