b208ddda485922bd3c023d00a310fca7b9fc3cdb
Hanas <hanasoke@gmail.com>
2026-02-11 22:39:19 +0700

    updated seller_page page

--- database/e-commerce.rb
+++ database/e-commerce.rb
@@ -168,35 +168,3 @@ DB.execute <<-SQL
 SQL
 
 # DB.execute("DROP TABLE transactions");
-
-DB.execute <<-SQL
-    CREATE TABLE IF NOT EXISTS shipments (
-        shipping_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        transaction_id INTEGER,
-        shipping_status TEXT,
-        shipping_date TEXT,
-        FOREIGN KEY(transaction_id) REFERENCES transactions(transaction_id)
-    );
-SQL
-
-# DB.execute("DROP TABLE shipments");
-
-DB.execute <<-SQL 
-    CREATE TABLE IF NOT EXISTS messages (
-        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        store_id INTEGER,
-        user_id INTEGER,
-        sender_type TEXT, -- 'user' or 'seller'
-        message_text TEXT,
-        message_type TEXT DEFAULT 'text', -- 'text', 'image', 'product'
-        product_id INTEGER, -- if message_type is 'product'
-        image_path TEXT, -- if message_type is 'image'
-        is_read BOOLEAN DEFAULT FALSE,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY(store_id) REFERENCES stores(store_id),
-        FOREIGN KEY(user_id) REFERENCES users(user_id),
-        FOREIGN KEY(product_id) REFERENCES items(item_id)
-    );
-SQL
-
-# DB.execute("DROP TABLE messages");
